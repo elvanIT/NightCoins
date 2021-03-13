49bf8a0305286c78bae745ebfa8d3b9c39834922
pavel <pavelanyr@gmail.com>
2021-03-13 13:16:21 +0300

    clicker
    clicker style and work

---
+++ script.js
@@ -0,0 +1,18 @@
+var score = 0;
+
+function clickBtn() {
+  score++;
+  console.log(score);
+  document.getElementsByTagName("p")[0].firstChild.data = "Clicks: " + score;
+}
+
+function loadPage() {
+  $("#background").animate({}, 800, function() {
+    $("#clickbtn").animate({height: "250px", width: "250px"}, 850, function() {
+      $("#clickbtn").animate({fontSize: "45px"}, 850, function() {
+      $("#score").animate({fontSize: "40px"}, 850, function() {
+      })
+      })
+    })
+  })
+}
